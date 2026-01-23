8bddf2591a32c27331eeb156d69d12d5748d9bc5
LokiLogic <tamas.lakatos1@student.uni-miskolc.hu>
2026-01-23 18:25:15 -0500

    add react-challenge-day10

---
+++ react-challenge-day10/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css';
+
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p>&copy; 2026 ReactChallenge. All rights reserved.</p>
+            <div className='socials'>
+                <span>Facebook</span>
+                <span>Twitter</span>
+                <span>Instagram</span>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
